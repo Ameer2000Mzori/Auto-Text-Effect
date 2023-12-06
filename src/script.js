4b13d2f9aefd06299f110d6a40060a254391eb2b
Ameer <ameerapex496@gmail.com>
2023-12-06 23:50:24 +0100

    added function to the project, auto type function

--- src/script.js
+++ src/script.js
@@ -1,1 +1,25 @@
-console.log("hallo world");
+var titleText = document.getElementsByClassName("title-Text")[0];
+var textEl = "WE LOVE PROGRAMMING";
+var speed = 100;
+var index = 0;
+var isReversing = false;
+var writeText = function () {
+    if (!isReversing) {
+        titleText.innerText = textEl.slice(0, index);
+        index++;
+        if (index > textEl.length) {
+            isReversing = true;
+            index = textEl.length - 1;
+        }
+    }
+    else {
+        titleText.innerText = textEl.slice(0, index);
+        index--;
+        if (index < 0) {
+            isReversing = false;
+            index = 0;
+        }
+    }
+    setTimeout(writeText, speed);
+};
+writeText();
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,30 @@
-console.log("hallo world");
+let titleText: any = document.getElementsByClassName("title-Text")[0];
+
+let textEl: string = `WE LOVE PROGRAMMING`;
+let speed = 100;
+let index = 0;
+let isReversing = false;
+
+const writeText = () => {
+  if (!isReversing) {
+    titleText.innerText = textEl.slice(0, index);
+    index++;
+
+    if (index > textEl.length) {
+      isReversing = true;
+      index = textEl.length - 1;
+    }
+  } else {
+    titleText.innerText = textEl.slice(0, index);
+    index--;
+
+    if (index < 0) {
+      isReversing = false;
+      index = 0;
+    }
+  }
+
+  setTimeout(writeText, speed);
+};
+
+writeText();
